9054dec2bed14e79cb07426ad714237f51ea7489
nikhilramesh <nikhilramesh@192.168.1.145>
2019-02-25 20:05:49 -0700

    Version 1 for CSE 360 assignment2

---
+++ CSE360HW2/src/cse360assign2/Calculator.java
@@ -0,0 +1,34 @@
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {			//returns value of total
+		return 0;
+	}
+	
+	public void add (int value) {		//adds parameter value to total value
+		
+	}
+	
+	public void subtract (int value) {	//subtracts parameter value from total value
+		
+	}
+	
+	public void multiply (int value) {	//multiplies parameter value with total value
+		
+	}
+	
+	public void divide (int value) {	//divides total value by parameter value
+		
+	}
+	
+	public String getHistory () {		//lists operands called on
+		return "";
+	}
+}
