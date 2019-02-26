f7a7192a1db85362dfa1005faf5ce0ccd0391dcc
nikhilramesh <nikhil.ramesh08@gmail.com>
2019-02-25 21:16:20 -0700

    Version 3

--- CSE360HW2/src/cse360assign2/Calculator.java
+++ CSE360HW2/src/cse360assign2/Calculator.java
@@ -3,25 +3,31 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private StringBuffer hold;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		hold = new StringBuffer("0 "); //initial value of total is 0 and is displayed as such
 	}
 	
 	public int getTotal () {			//returns value of total
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {		//adds parameter value to total value
 		total = total + value;
+		hold.append(" + " + value);
+		
 	}
 	
 	public void subtract (int value) {	//subtracts parameter value from total value
 		total = total - value;
+		hold.append(" - " + value);
 	}
 	
 	public void multiply (int value) {	//multiplies parameter value with total value
 		total = total * value;
+		hold.append(" * " + value);
 	}
 	
 	public void divide (int value) {	//divides total value by parameter value
@@ -33,9 +39,11 @@ public class Calculator {
 		{
 			total = total / value;
 		}
+		hold.append(" / " + value);
 	}
 	
 	public String getHistory () {		//lists operands called on
-		return "";
+		
+		return hold.toString();
 	}
 }
